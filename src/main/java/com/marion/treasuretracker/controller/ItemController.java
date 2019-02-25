d2668e1e5935068e20c66c109cc06897484c46c4
Joe Marion <humantargetjoe@hotmail.com>
2019-02-24 21:24:55 -0500

    Added form for adding an item. Going to build the old-school form for expediency, needs more work for Item to support item types probably. Also need to build in option for container creation, and mapping an item to a container.

--- src/main/java/com/marion/treasuretracker/controller/ItemController.java
+++ src/main/java/com/marion/treasuretracker/controller/ItemController.java
@@ -1,7 +1,12 @@
 package com.marion.treasuretracker.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marion.treasuretracker.model.Item;
+import com.marion.treasuretracker.model.ItemSubType;
+import com.marion.treasuretracker.model.ItemType;
 import com.marion.treasuretracker.service.ItemService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +19,8 @@ import java.util.List;
 
 @Controller()
 public class ItemController {
+    private static Log log = LogFactory.getLog(ItemController.class);
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
     ItemService itemService;
@@ -22,6 +29,21 @@ public class ItemController {
     public String listItems(ModelMap model) {
         model.addAttribute("items", itemService.listItems());
         return "list-items";
+    }
+
+    @RequestMapping(value = "/add-item", method = RequestMethod.GET)
+    public String addItem(ModelMap model) {
+        model.addAttribute("item", new Item());
+        model.addAttribute("itemTypes", ItemType.values());
+        model.addAttribute("itemSubTypes", ItemSubType.values());
+        return "add-item";
+    }
+
+    @RequestMapping(value = "item/create", method = RequestMethod.POST)
+    public String saveProduct(Item item) throws Exception {
+        log.info(objectMapper.writeValueAsString(item));
+        itemService.createItem(item);
+        return "redirect:/list-items";
     }
 
     @RequestMapping(value = "/api/item", method = RequestMethod.POST, produces = "application/json")
